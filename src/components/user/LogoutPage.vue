aab0c7f93c7575bf5e22d8031ee99e05810211d8
NoPlans4Tonight <jlochan53@gmail.com>
2023-08-11 15:57:01 -0500

    logout page

---
+++ src/components/user/LogoutPage.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+      <!-- Your other content here -->
+  
+      <v-btn color="primary" @click="logout">Logout</v-btn>
+    </div>
+  </template>
+  
+  <script>
+  import axios from '@/axios';
+  
+  export default {
+    methods: {
+      async logout() {
+        try {
+          // Send a logout request to the backend
+          const response = await axios.post('/logout');
+  
+          // Assuming the logout was successful, clear client-side authentication state
+          if (response.data.status === 1) {
+            // Clear any authentication tokens or user information
+            // For example, if using Vuex, you might dispatch an action to clear the user state
+  
+            // Redirect the user to the login page
+            this.$router.push('/login');
+          } else {
+            // Handle failed logout
+            console.error('Logout failed:', response.data.message);
+          }
+        } catch (error) {
+          console.error('Logout failed:', error);
+        }
+      }
+    }
+  };
+  </script>
+  
