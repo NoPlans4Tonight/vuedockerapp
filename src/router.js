516448b208d9b54941eb709acd3716b1d5877335
NoPlans4Tonight <jlochan53@gmail.com>
2023-08-11 15:56:41 -0500

    update routes

--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from './components/HomePage.vue';
 import ProjectsPage from './components/projects/ProjectsPage.vue';
+import RegisterPage from './components/user/RegisterPage.vue';
+import LoginPage from './components/user/LoginPage.vue';
+import LogoutPage from './components/user/LogoutPage.vue';
 
 const routes = [
   {
@@ -13,6 +16,21 @@ const routes = [
     name: 'ProjectsPage',
     component: ProjectsPage,
   },
+  {
+    path: '/register',
+    name: 'RegisterPage',
+    component: RegisterPage,
+  },
+  {
+    path: '/login',
+    name: 'LoginPage',
+    component: LoginPage,
+  },
+  {
+    path: '/logout',
+    name: 'LogoutPage',
+    component: LogoutPage,
+  }
   // ... other routes
 ];
 
